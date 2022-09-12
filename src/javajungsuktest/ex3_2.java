47cbb61f46fd1c1bd60ac37b19e981e654c925f5
jinhee park <bis04199@gmail.com>
2022-09-12 22:23:25 +0900

    3.2

--- src/javajungsuktest/ex3_2.java
+++ src/javajungsuktest/ex3_2.java
@@ -6,7 +6,7 @@ public class ex3_2 {
 		int numOfApples = 123;//사과개수
 		int sizeOfBucket = 10; //바구니에 담을 수있는 사과의 개수
 		int numOfBucket =numOfApples/sizeOfBucket + (numOfApples%sizeOfBucket>0 ? 1 : 0);
-		System.out.println("필요한 바구니의 수 : "+sizeOfBucket);
+		System.out.println("필요한 바구니의 수 : "+numOfBucket);
 	}
 
 }
