47a91025dd6b367264c85317f7e0bfce2bf01cf8
jinhee park <bis04199@gmail.com>
2022-09-13 10:29:01 +0900

    3-4

---
+++ src/javajungsuktest/ex3_4.java
@@ -0,0 +1,11 @@
+package javajungsuktest;
+
+public class ex3_4 {
+
+	public static void main(String[] args) {
+		int num = 456;
+		System.out.println(num/100*100);
+	}
+
+}
+//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
