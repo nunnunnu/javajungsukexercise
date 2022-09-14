f1e65d16a620d7a0a5eda586c6601db21c984d09
jinhee park <bis04199@gmail.com>
2022-09-14 22:53:36 +0900

    4-5

--- src/javajungsuktestEx4/ex4_4.java
+++ src/javajungsuktestEx4/ex4_4.java
@@ -2,16 +2,28 @@ package javajungsuktestEx4;
 
 public class ex4_4 {
 	public static void main(String[] args) {
+		//1+(-2)+3+(-4)+... 같은 식으로 계속 더했을때 몇까지 더해야 총합이 100이상이 되는지 구하시오
 		int sum = 0;
 		int count = 0;
 		for(int i=1; true; i++) {
 			if(i%2==0) sum -=i;
 			else sum +=i;
 			count++;
-			if(sum>=100) 
+			if(sum>=10) 
 				break;
 		}
 		System.out.println("count = "+count);
 		System.out.println("sum = "+sum);
+		
+//		//문제집 정답
+//		int sum = 0, s=1, num = 0;
+//		for(int i=1;true;i++,s=-s) { //반복마다 1,-1,1,-1.. 
+//			num=i*s; //더할 값을 구함
+//			sum += num;
+//			
+//			if(sum>=100) break;
+//		}
+//		System.out.println("num = "+num);
+//		System.out.println("sum = "+sum);
 	}
 } 
---
+++ src/javajungsuktestEx4/ex4_5.java
@@ -0,0 +1,24 @@
+package javajungsuktestEx4;
+
+public class ex4_5 {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=10;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		//위 for문을 while문으로 변경
+		int i=0;
+		while(i<=10) {
+			int j = 0;
+			while(j<=i) {
+				System.out.print("*");
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+}
