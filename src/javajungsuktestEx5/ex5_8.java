ce0cadceb047458724aef93ec71d52ea0a51a0a4
jinhee park <bis04199@gmail.com>
2022-09-16 22:49:39 +0900

    5-8

---
+++ src/javajungsuktestEx5/ex5_8.java
@@ -0,0 +1,39 @@
+package javajungsuktestEx5;
+
+public class ex5_8 {
+
+	public static void main(String[] args) {
+		//answer의 각 숫자 개수를 세서 개수만큼 '*'를 찍어서 그래프 그리기
+//		int answer[] = {1,4,4,3,1,4,4,2,1,3,2};
+//		int counter[] = new int[4];
+//		
+//		for(int i=0;i<counter.length;i++) {
+//			int count = 0;
+//			for(int j=0;j<answer.length;j++) {
+//				if(i+1==answer[j]) count++;
+//			}
+//			counter[i] = count;
+//			System.out.print(counter[i]);
+//			for(int x=0;x<counter[i];x++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		//모르겠어서 걍 연습문제 코드 무시하고 만들어봄ㅜㅜㅜ
+		int answer[] = {1,4,4,3,1,4,4,2,1,3,2};
+		int counter[] = new int[4];
+		
+		for(int i=0;i<answer.length;i++) {
+			counter[answer[i]-1]++; //배열의 인덱스(0~3)를 구하려면 배열크기에서 -1해줘야함
+		}
+		for(int i=0; i < counter.length;i++) {
+			System.out.print(counter[i]);
+			for(int j=0; j < counter[i];j++) {
+			System.out.print("*"); 
+			}
+		System.out.println();
+		}
+		
+	}
+
+}
