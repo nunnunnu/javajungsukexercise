01e0528174b6f0d598bee4fc5146044c8b5f8acc
jinhee park <bis04199@gmail.com>
2022-09-17 19:13:37 +0900

    6-3~6-4

---
+++ src/javajungsuktestEx6/ex6_4.java
@@ -0,0 +1,37 @@
+package javajungsuktestEx6;
+
+class Student {
+	String name;
+	int ban;
+	int no;
+	int kor;
+	int eng;
+	int math;
+	
+	int getTotal() {
+		return kor + eng + math;
+	}
+	float getAverage() {
+		return (Math.round(((getTotal())/3f)*10))/10f;
+	}
+}
+
+public class ex6_4 {
+
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.name="홍길동";
+		s.ban=1;
+		s.no=1;
+		s.kor=100;
+		s.eng=60;
+		s.math=76;
+		
+		System.out.println("이름 : "+s.name);
+		System.out.println("총점 : "+s.getTotal());
+		System.out.println("평균 : "+s.getAverage());
+		
+		
+	}
+
+}
