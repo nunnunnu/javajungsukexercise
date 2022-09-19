7d8b0a9e127b50f75200a39d0dc460355ff1bba5
jinhee park <bis04199@gmail.com>
2022-09-19 22:18:33 +0900

    7-19

--- src/javajungsuktestEx7/ex7_19.java
+++ src/javajungsuktestEx7/ex7_19.java
@@ -4,56 +4,56 @@ import java.util.Arrays;
 
 class Buyer{
 	int money = 1000;
-	Product cart[] = new Product[3];
-	int i; //cart의 인덱스
+	Product1 cart[] = new Product1[3];
+	int i=0; //cart의 인덱스
 	
-	void buy(Product p) {
-		if(money<p.price) {
-			System.out.println("잔액이 부족하여 "+p+"를 살 수 없습니다.");
+	void buy(Product1 p) {
+		if(money < p.price) {
+			System.out.println("잔액부족. "+ p +"구매 불가");
 			return;
-		}else {
-			money -= p.price;
-			add(p);
 		}
+		money-=p.price;
+		add(p);
 	}
-	void add(Product p) {
+	void add(Product1 p) {
 		if(i>=cart.length) {
-			Product tmp[] = new Product[cart.length*2];
+			Product1 tmp[] = new Product1[cart.length*2];
 			System.arraycopy(cart, 0, tmp, 0, cart.length);
-			cart = tmp;	
+			cart = tmp;
 		}
 		cart[i++] = p;
 	}
 	void summary() {
-		String itemList="";
-		int sum =0;
+		String itemList = "";
+		int sum = 0;
 		for(int i=0;i<cart.length;i++) {
 			if(cart[i]==null) break;
-			itemList += cart[i] +", ";
-			sum+=cart[i].price;
+			
+			itemList += cart[i]+", ";
+			sum += cart[i].price;
 		}
-		System.out.println("구입한 물건 : "+itemList);
-		System.out.println("사용한 금액 : "+sum);
+		System.out.println("구매 목록 : "+itemList);
+		System.out.println("물건 가격 : "+sum);
 		System.out.println("남은 금액 : "+money);
 	}
 }
 
-class Product{
+class Product1{
 	int price;
 	
-	Product(int price) {
+	Product1(int price) {
 		this.price = price;
 	}
 }
-class Tv extends Product{
-	Tv(){ super(100);}
+class Tv1 extends Product1{
+	Tv1(){ super(100);}
 	public String toString() { return "Tv";}
 }
-class Computer extends Product{
+class Computer extends Product1{
 	Computer(){ super(200);}
 	public String toString() { return "Computer";}
 }
-class Audio extends Product{
+class Audio extends Product1{
 	Audio(){ super(50);}
 	public String toString() { return "Audio";}
 }
@@ -61,14 +61,14 @@ public class ex7_19 {
 
 	public static void main(String[] args) {
 		Buyer b = new Buyer();
-		b.buy(new Tv());
+		b.buy(new Tv1());
 		b.buy(new Computer());
-		b.buy(new Tv());
+		b.buy(new Tv1());
 		b.buy(new Audio());
 		b.buy(new Computer());
 		b.buy(new Computer());
 		b.buy(new Computer());
-				
 		b.summary();
+		
 	}
 }
