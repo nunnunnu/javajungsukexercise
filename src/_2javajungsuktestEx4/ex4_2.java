4d51713ed46878dc2ae24aa253c81b6e91cf6862
jinhee park <bis04199@gmail.com>
2022-10-02 14:58:12 +0900

    4-2

---
+++ src/_2javajungsuktestEx4/ex4_2.java
@@ -0,0 +1,15 @@
+package _2javajungsuktestEx4;
+
+public class ex4_2 {
+
+	public static void main(String[] args) {
+		//1~20중 2또는 3의 배수가 아닌 수의 총합
+		int sum=0;
+		for(int i=1;i<=20;i++) {
+			if(!(i%2==0 || i%3==0))
+				sum+=i;
+		}
+		System.out.println(sum);
+	}
+
+}
